2c2b07496f9e7ffb7fb47a4f575b79480ac09e9c
Amauri Carpio <a.robertocarpio@gmail.com>
2026-02-28 15:07:17 -0600

    Se agrega fix en routes password

--- src/modules/password/password.routes.js
+++ src/modules/password/password.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const passwordController = require('./password.controller');
 
-router.put('/usuarios/password/email', passwordController.cambiarPorEmail);
+router.put('/email', passwordController.cambiarPorEmail);
 
 module.exports = router;
